71488de5e69b204c7d6f3240a8e7082c34f9e83f
akankshipriya <akankshipriya24@gmail.com>
2021-08-02 07:51:38 +0530

    first question updation

--- DaysCalculate/DaysCalculate/Program.cs
+++ DaysCalculate/DaysCalculate/Program.cs
@@ -19,14 +19,42 @@ namespace DaysCalculate
             Console.WriteLine("Enter Date in Given Format:(YYYY/MM/DD)");
             DateTime FromDate;
             DateTime toDate;
-            Console.WriteLine("Please Enter valid fromdate");
+            x:Console.WriteLine("Please Enter valid from date");
             FromDate = Convert.ToDateTime(Console.ReadLine());
-            Console.WriteLine("Please Enter valid todate");
+            if (validFromDate(FromDate)!=true)
+            {
+                goto x;
+            }
+           
+            y:Console.WriteLine("Please Enter valid to date");
             toDate = Convert.ToDateTime(Console.ReadLine());
+            if (!validToDate(toDate,FromDate))
+                {
+                goto y;
+            }
+            
             TimeSpan difference = toDate - FromDate;
             int noOfDays = difference.Days;
             Console.WriteLine(noOfDays);
 
+
+        }
+        static bool validFromDate(DateTime from)
+        {
+            if (from<DateTime.Today)
+            {
+                return true;
+            }
+            return false;
+
+        }
+        static bool validToDate(DateTime to, DateTime from)
+        {
+            if (to>=from)
+                {
+                return true;
+                }
+            return false;
 
         }
     }
